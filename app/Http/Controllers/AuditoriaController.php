4ff0f439c22e198fc41afec65c805449ad2f84d1
alejandro27647120 <jasve504@gmail.com>
2021-04-12 10:18:01 -0400

    falta por terminar de arreglar el mostrar la audit

--- app/Http/Controllers/AuditoriaController.php
+++ app/Http/Controllers/AuditoriaController.php
@@ -10,17 +10,11 @@ use App\auditoria;
 class AuditoriaController extends Controller
 {
     public function index(Request $request){
+
         $busqueda = $request->get('buscar');
-        $datos['audit'] = usuarios::JOIN("auditorias", "auditorias.au_id", "=", "usuarios.us_id")
-                                   ->Where('usuarios.us_cedula', '=', $busqueda)
+        $datos['audit'] = auditoria::JOIN("usuarios", "usuarios.us_id", "=", "auditorias.au_us_id")
+                                   ->Where("us_cedula", "=", $busqueda)
                                    ->Paginate(10);
-
-        /*$busqueda = $request->get('buscar');
-
-        $busqueda = $request->get('buscar');
-        $datos['audit']= auditoria::JOIN('usuarios', 'usuarios.us_id', '=', 'auditorias.au_us_id')
-                                  ->Where('usuarios.us_cedula', '=', $busqueda)
-                                  ->paginate(5);*/
         return view('mconfig.auditoria', $datos);
     }
 }
